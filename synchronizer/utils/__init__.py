d7a5b759da9a666ede793cb99094f2406bb43bb8
Pavel Pantyukhov <pantyukhov.p@gmail.com>
2021-10-20 18:22:43 +0300

    Update __init__.py

--- synchronizer/utils/__init__.py
+++ synchronizer/utils/__init__.py
@@ -1,10 +1,10 @@
 from unidecode import unidecode
-from django.utils import six
+from six import text_type
 from django.utils.text import slugify as django_slugify
 
 
 def slugifier(value, allow_unicode=False):
     if not allow_unicode:
-        value = unidecode(six.text_type(value))
+        value = unidecode(text_type(value))
 
     return django_slugify(value, allow_unicode=allow_unicode)
